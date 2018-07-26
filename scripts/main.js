1a7ae4009758e8cee4ff8adf9855353ef79d2f39
Weis <22601983+Mlk777@users.noreply.github.com>
2018-07-26 15:39:30 +0200

    Changing functions and implemented ES6

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,40 @@
-////////////////////////
-//API call
-$(document).ready(function() {
-  count = 0;
-  $("#newQuote").on("click", function() {
-    $(".btn").prop("disabled", true);
-    count++;
-    console.log(count);
-    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(json){
-      $("#quote").empty();
-      $(".message").empty();
-      $("#quote").fadeIn("slow");
-      $(".message").fadeIn("slow");
-      Object.keys(json).forEach(function(key) {
+$(document).ready(function () {
+  $("#newQuote").on("click", function () {
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function (json) {
+      (() => {
+        $(".btn").prop("disabled", true);
+        $("#quote").empty();
+        $(".message").empty();
+        $("#quote").fadeIn("slow");
+        $(".message").fadeIn("slow");
+      })();
+      Object.keys(json).forEach(function (key) {
         var quote = new Typed('#quote', {
           strings: [json.quoteText],
           typeSpeed: -10,
           fadeOut: true,
           loop: false,
           showCursor: false
-         });
-        $("#twitterButton").attr("href", 'https://twitter.com/intent/tweet?text=Quote of the day :' + json.quoteText+ '%0a' + json.quoteAuthor);
-        setTimeout(function() {
+        });
+        $("#twitterButton").attr("href", 'https://twitter.com/intent/tweet?text=Quote of the day :' + json.quoteText + '%0a' + json.quoteAuthor);
+        setTimeout(() => {
           var author = new Typed('.message', {
-          strings: [json.quoteAuthor],
-          typeSpeed: 10,
-          fadeOut: true,
-          loop: false,
-          showCursor: false
+            strings: [json.quoteAuthor],
+            typeSpeed: 10,
+            fadeOut: true,
+            loop: false,
+            showCursor: false
           });
         }, 1800);
       });
-      setTimeout(function() {
+      setTimeout(() => {
+        (() => {
           $(".btn").prop("disabled", false);
           $(".etchBorder").effect("shake");
           $("#quote").fadeOut("slow");
           $(".message").fadeOut("slow");
-        }, 6500);
+        })();
+      }, 6500);
     });
   });
 });
