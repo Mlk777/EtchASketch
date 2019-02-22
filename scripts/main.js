eae17d213f2e88f15aa75d77ff5909614f63ee05
mlk777 <>
2019-02-22 20:04:05 +0100

    Fixing shake effect

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,51 @@
-$(document).ready(function () {
-  $("#newQuote").on("click", function () {
-    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function (json) {
-      (() => {
-        $(".btn").prop("disabled", true);
-        $("#quote").empty();
-        $(".message").empty();
-        $("#quote").fadeIn("slow");
-        $(".message").fadeIn("slow");
-      })();
-      Object.keys(json).forEach(function (key) {
-        var quote = new Typed('#quote', {
-          strings: [json.quoteText],
-          typeSpeed: -10,
-          fadeOut: true,
-          loop: false,
-          showCursor: false
-        });
-        $("#twitterButton").attr("href", 'https://twitter.com/intent/tweet?text=Quote of the day :' + json.quoteText + '%0a' + json.quoteAuthor);
-        setTimeout(() => {
-          var author = new Typed('.message', {
-            strings: [json.quoteAuthor],
-            typeSpeed: 10,
+$(document).ready(function() {
+  $("#newQuote").on("click", function() {
+    $.getJSON(
+      "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+      function(json) {
+        (() => {
+          $(".btn").prop("disabled", true);
+          $("#quote").empty();
+          $(".message").empty();
+          $("#quote").fadeIn("slow");
+          $(".message").fadeIn("slow");
+          $(".my-4").fadeTo("slow", 0.3);
+        })();
+        Object.keys(json).forEach(function(key) {
+          var quote = new Typed("#quote", {
+            strings: [json.quoteText],
+            typeSpeed: -10,
             fadeOut: true,
             loop: false,
             showCursor: false
           });
-        }, 1800);
-      });
-      setTimeout(() => {
-        (() => {
-          $(".btn").prop("disabled", false);
-          $(".etchBorder").effect("shake");
-          $("#quote").fadeOut("slow");
-          $(".message").fadeOut("slow");
-        })();
-      }, 6500);
-    });
+          $("#twitterButton").attr(
+            "href",
+            "https://twitter.com/intent/tweet?text=Quote of the day :" +
+              json.quoteText +
+              "%0a" +
+              json.quoteAuthor
+          );
+          setTimeout(() => {
+            var author = new Typed(".message", {
+              strings: [json.quoteAuthor],
+              typeSpeed: 10,
+              fadeOut: true,
+              loop: false,
+              showCursor: false
+            });
+          }, 1800);
+        });
+        setTimeout(() => {
+          (() => {
+            $(".btn").prop("disabled", false);
+            $(".etchBorder").effect("shake");
+            $("#quote").fadeOut("slow");
+            $(".message").fadeOut("slow");
+            $(".my-4").fadeOut("slow");
+          })();
+        }, 6500);
+      }
+    );
   });
 });
